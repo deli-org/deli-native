bfb475f4f89f362920c72d945ba0782e59aad45e
gil-air-may <gilairmay@gmail.com>
2020-12-15 19:57:11 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { InputItem, Button, Provider } from '@ant-design/react-native'
+import { useState, useEffect } from 'react'
+import * as Font from 'expo-font'
+import { api } from './app/utils'
+import AppLoading from 'expo-app-loading'
 
-export default function App() {
+const loadFonts = async () => {
+  await Font.loadAsync(
+    'antoutline',
+    // eslint-disable-next-line
+    require('@ant-design/icons-react-native/fonts/antoutline.ttf')
+  );
+
+  await Font.loadAsync(
+    'antfill',
+    // eslint-disable-next-line
+    require('@ant-design/icons-react-native/fonts/antfill.ttf')
+  );
+}
+
+export const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const submit = () => {
+    api.post('/login/', { username, password }).then(
+      response => {
+        console.log(response.data)
+      }
+    )
+  }
+
+  useEffect(() => {
+    loadFonts()
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <InputItem
+        value={username}
+        placeholder="username"
+        onChange={(value) => setUsername(value)}
+        type="text">
+      </InputItem>
+      <InputItem
+        type="password"
+        value={password}
+        placeholder="password"
+        onChange={(value) => setPassword(value)}>
+      </InputItem>
+      <Button onPress={() => submit()}>
+        Submit
+      </Button>
     </View>
   );
 }
@@ -19,3 +68,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
